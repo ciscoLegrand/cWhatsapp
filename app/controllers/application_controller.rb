4f9ae6e50826952eee3f455c196792e0bc2883c1
ciscoLegrand <cisco.glez@gmail.com>
2022-10-14 12:00:57 +0200

    [fix] nil cookies[:data] setting token whit default value

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_token
+  private
+  def set_token
+    return cookies[:data] if defined? cookies[:data]
+
+    cookies[:data] ||= 'no hay token'
+  end
 end
